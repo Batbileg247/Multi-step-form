651e87120c08d04b57a15e4128ff87e69b39f456
25lp3625 User <25lp3625@administrators-iMac.local>
2026-01-21 12:14:57 +0800

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,65 +1,42 @@
-import Image from "next/image";
+'use client'
+
+import { useState } from "react"
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+  const [value, setValue] = useState("")
+
+  const click = () => {
+    console.log(value)
+    setValue("")
+  }
+
+  const handleChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return (<div className="w-screen h-screen flex justify-center items-center ">
+    <div className="bg-white w-120 p-8 rounded-lg">
+      <header>
+        <img className="h-15 w-15" src="/main.png" alt="logo" />
+        <h1 className="text-[26px] font-semibold mt-2">Join Us! 😎</h1>
+        <h2 className="text-[#8E8E8E] text-lg mt-2">Please provide all current information accurately.</h2>
+      </header>
+      <section className="mt-7 flex flex-col justify-between min-h-105">
+        <div className="flex flex-col gap-3">
+          {<div>
+            <div className="flex text-sm gap-1 font-semibold">
+              <p className="">First name</p><p className="text-[#E14942]">*</p>
+            </div>
+            <input className="border border-[#CBD5E1] mt-2 p-3 w-full h-11 rounded-lg"
+              type="text"
+              onChange={handleChange}
+              placeholder="Your first name"
+              value={value} />
+          </div>}
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+        <button onClick={click} className="rounded-md cursor-pointer gap-2 flex items-center justify-center text-base font-semibold bg-[#121316] text-white w-full h-11">Continue 1/3 <img className="h-5" src="/arrow.png" alt="arrow" /></button>
+      </section>
     </div>
+  </div>
   );
 }
